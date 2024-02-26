174cf599a6d864e5e71e0b1bc0268895335ef149
ankithshetty17 <ankishetty2002@gmail.com>
2024-02-26 23:36:09 +0530

    update

--- src/components/triage.js
+++ src/components/triage.js
@@ -92,8 +92,9 @@ const TriageForm = () => {
           <label htmlFor="massCasualtySelect" className="block text-gray-700 text-sm font-bold mb-2 w-64">
             Is the patient a victim of mass casualty
           </label>
-          <select id="massCasualtySelect" className="block appearance-none w-32 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
-            <option value="selectoption">Select Option</option>
+          <select id="massCasualtySelect" className="block appearance-none w-56 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+           <option value="selectoption">Select Option</option> 
+
             <option value="yes">Yes</option>
             <option value="no">No</option>
           </select>
@@ -108,10 +109,10 @@ const TriageForm = () => {
              type="number"/>
           </div>
           <div className="mb-4">
-          <label htmlFor="gender" className="block text-gray-700 text-sm font-bold mb-2 w-64">
+          <label htmlFor="gender" className="flex text-gray-700 text-sm font-bold mb-2 w-64">
             Gender
           </label>
-          <select id="gender" className="block appearance-none w-32 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+          <select id="gender" className="block appearance-none w-56 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
             <option value="selectoption">Select Option</option>
             <option value="male">Male</option> 
             <option value="female">Female</option>
@@ -151,10 +152,10 @@ const TriageForm = () => {
             />
           </div>
           <div className="mb-4">
-          <label htmlFor="maritialStatus" className="block text-gray-700 text-sm font-bold mb-2 w-64">
+          <label htmlFor="maritialStatus" className="flex text-gray-700 text-sm font-bold mb-2 w-64">
            Maritial Status
           </label>
-          <select id="maritialStatus" className="block appearance-none w-32 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+          <select id="maritialStatus" className="block appearance-none w-56 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
             <option value="selectoption">Select Option</option>
             <option value="single">Single</option> 
             <option value="married">Married</option>
@@ -167,8 +168,9 @@ const TriageForm = () => {
             <label htmlFor='address' className="flex text-gray-700 text-sm font-bold mb-2 w-96">
               Address
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-            id='address'
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              rows="3"
             />
           </div>
         </div>
@@ -220,7 +222,7 @@ const TriageForm = () => {
             />
           </div>
           <div className="mb-4">
-          <label htmlFor="nurseSendTo" className="flex text-gray-700 text-sm font-bold mb-2 w-96">
+          <label htmlFor="nurseSendTo" className="flex text-gray-700 text-sm font-bold mb-2 w-52">
           Nurse: Send To
           </label>
           <select id="nurseSendTo" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
@@ -261,10 +263,10 @@ const TriageForm = () => {
             />
           </div>
           <div className="mb-4 ">
-            <label htmlFor="visitortag" className="flex text-gray-700 text-sm font-bold mb-2 w-96">
+            <label htmlFor="visitortag" className="flex text-gray-700 text-sm font-bold mb-2 w-56">
               Visitor id tag provided
             </label>
-            <select id="visitortag" className="block appearance-none w-32 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+            <select id="visitortag" className="block appearance-none w-56 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
             <option value="selectoption">Select Option</option>
             <option value="yes">Yes</option>
             <option value="no">No</option>
@@ -272,10 +274,10 @@ const TriageForm = () => {
         </div>
         
           <div className="mb-4">
-            <label htmlFor='registrationDesk' className="flex text-gray-700 text-sm font-bold mb-2 w-56">
+            <label htmlFor='registrationDesk' className="flex text-gray-700 text-sm font-bold mb-2 w-64">
               Register desk activated
             </label>
-            <select id="registrationDesk" className="block appearance-none w-32 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+            <select id="registrationDesk" className="block appearance-none w-64 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
             <option value="selectoption">Select Option</option>
             <option value="yes">Yes</option>
             <option value="no">No</option>  
@@ -307,43 +309,87 @@ const TriageForm = () => {
           Physical Triage
         </h1>
         <div className="block sm:flex md:flex xl:flex justify-start max-w-md pl-6 space-x-12 pt-8 ml-9">
-          <div className="mb-4 ">
+        <div className="mb-4 ">
             <label className=" text-gray-700 text-sm font-bold mb-2 w-36  sm:w-56  sm:flex">
-              Physical Triage Date and Time
+             Physical Triage Date
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               //   id="name"
-              //   type="text"
+                type="date"
+              //   name="name"
+            />
+          </div>
+          <div className="mb-4 ">
+            <label className=" text-gray-700 text-sm font-bold mb-2 w-36  sm:w-56  sm:flex">
+             Physical Triage Time
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              //   id="name"
+                type="time"
               //   name="name"
             />
           </div>
           <div className="mb-4">
-            <label className="flex text-gray-700 text-sm font-bold mb-2 w-96">
-              Emergency Physician Triage
+          <label htmlFor="emergencyPhysicianTriage" className="flex text-gray-700 text-sm font-bold mb-2 w-96">
+          Emergency Physician Triage
+          </label>
+          <select id="emergencyPhysicianTriage" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+            <option value="selectOption" className="text-white bg-blue-600">Select Option</option>
+            <option value="priority1" className="text-white bg-blue-600">Patient Dying: Priority 1</option>
+            <option value="priority2" className="text-white bg-red-600">Cannot Wait: Priority 2</option>
+            <option value="priority3" className="text-white bg-yellow-300">Needs Multiple Resources:  Priority 3</option>
+            <option value="priority4" className="text-white bg-red-600">Danger Zone vitals present:  Priority 2</option>
+            <option value="priority5" className="text-white bg-green-600">Needs single Resources:  Priority 4</option>
+            <option value="dead" className="text-black bg-white">Needs no resource: Priority 5</option>
+          </select>
+        </div>
+        <div className="mb-4">
+            <label htmlFor='EMConcurence' className="flex text-gray-700 text-sm font-bold mb-2 w-80">
+            EM Physician Concurence with nurse triage
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline space-x-20" />
-          </div>
-          <div className="mb-4">
-            <label className="flex text-gray-700 text-sm font-bold mb-2 w-96">
-              EM Physician Concurence with nurse triage
-            </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-          </div>
-          <div className="mb-4">
-            <label className="flex text-gray-700 text-sm font-bold mb-2 w-96">
-              Physician:Admission
-            </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-          </div>
+            <select id="EMConcurence" className="block appearance-none w-64 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+            <option value="selectoption">Select Option</option>
+            <option value="yes">Yes</option>
+            <option value="no">No</option>  
+            </select>     
+           </div>
+           <div className="mb-4">
+          <label htmlFor="PhysicianAdmission" className="flex text-gray-700 text-sm font-bold mb-2 w-24">
+          Physician:Admission
+          </label>
+          <select id="PhysicianAdmission" className="block appearance-none w-56 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+            <option value="selectOption" className="text-white bg-blue-600">Select Option</option>
+            <option value="priority1" className="text-white bg-blue-600">Blue Area: Priority 1</option>
+            <option value="priority2" className="text-white bg-red-600">Red Area: Priority 2</option>
+            <option value="priority3" className="text-white bg-yellow-300">Yellow Area:  Priority 3</option>
+            <option value="priority4" className="text-white bg-green-600">Green Area:  Priority 4</option>
+            <option value="priority5" className="text-white bg-orange-600">ED crit</option>
+            <option value="priority5" className="text-white bg-slate-400">ED Office</option>
+            <option value="priority5" className="text-white bg-cyan-600">Isolation</option>
+            <option value="priority5" className="text-white bg-black">Morque</option>
+            
+          </select>
+        </div>
         </div>
         <div className=" inline-block sm:flex md:flex xl:flex justify-start max-w-md pl-14 space-x-12 pt-8  ">
-          <div className="mb-4">
-            <label className="flex text-gray-700 text-sm font-bold mb-2 w-96">
-              Emergency severity index physician Triage code:Emergency Physician
-            </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-          </div>
+        <div className="mb-4">
+          <label htmlFor="emergencySecurityIndexPhysician" className="flex text-gray-700 text-sm font-bold mb-2 w-96">
+          Emergency severity index physician Triage code:Emergency Physician
+          </label>
+          <select id="emergencySecurityIndexPhysician" className="block appearance-none w-96 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+            <option value="selectOption" className="text-white bg-blue-600">Select Option</option>
+            <option value="priority1" className="text-white bg-blue-600">Patient Dying: Priority 1</option>
+            <option value="priority2" className="text-white bg-red-600">Can not Wait: Priority 2</option>
+            <option value="priority3" className="text-white bg-yellow-300">Needs Multiple resources:  Priority 3</option>
+            <option value="priority4" className="text-white bg-green-600">Needs one resurce:  Priority 4</option>
+            <option value="priority5" className="text-black bg-white">Needs no Resource: Priority 5</option>
+            <option value="priority5" className="text-white bg-black">Brought dead</option>
+            
+          </select>
+        </div>
+          
           <div className="mb-4">
             <label className="flex text-gray-700 text-sm font-bold mb-2 w-96 ">
               Remarks for Triage
@@ -353,18 +399,30 @@ const TriageForm = () => {
               rows="4"
             />
           </div>
-          <div className="mb-4 ">
-            <label className="flex text-gray-700 text-sm font-bold mb-2 w-96">
-              Major classification of case
-            </label>
-            <input className=" shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-          </div>
           <div className="mb-4">
-            <label className="flex text-gray-700 text-sm font-bold mb-2 w-56">
-              medicalogical case
+            <label htmlFor='Majorclassification' className="flex text-gray-700 text-sm font-bold mb-2 w-80">
+            Major classification of case
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-          </div>
+            <select id="Majorclassification" className="block appearance-none w-64 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+            <option value="selectoption">Select Option</option>
+            <option value="yes">Trauma</option>
+            <option value="no">Non-Trauma</option>  
+            <option value="yes">Trauma plus with medical conditon</option>
+            <option value="yes">Neonatal</option>
+            <option value="yes">Pediatric</option>
+            <option value="yes">Obstertic</option>
+            </select>     
+           </div>
+           <div className="mb-4">
+            <label htmlFor='medicalogicalcase' className="flex text-gray-700 text-sm font-bold mb-2 w-80">
+            medicalogical case
+            </label>
+            <select id="medicalogicalcase" className="block appearance-none w-64 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+            <option value="selectoption">Select Option</option>
+            <option value="yes">yes</option>
+            <option value="no">No</option>  
+            </select>     
+           </div>
         </div>
 
         <div className="button flex justify-center  pt-2 text-sm font-bold  pl-9 ">
